a9ecac24d61e9f8cdc2b8e3e17d96cb5b31730fa
Hernan <dropimax@gmail.com>
2017-11-11 12:45:49 -0500

    servicio imagen

--- app/src/main/java/com/catched_movil/app/Vista/ConsultaEventos.java
+++ app/src/main/java/com/catched_movil/app/Vista/ConsultaEventos.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -42,6 +43,7 @@ public class ConsultaEventos extends android.app.Fragment implements View.OnClic
     private Funciones obj_funciones;
     private ListAdapter adapter;
     private ListView v_lista;
+    private String cod_evento;
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -50,7 +52,22 @@ public class ConsultaEventos extends android.app.Fragment implements View.OnClic
         context = myview.getContext();
         obj_funciones              = new Funciones();
         v_lista = (ListView)       myview.findViewById(R.id.listViewEventos);
+
         new ConsultaEventos.Servicio_validar().execute();
+
+        v_lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+                cod_evento= ((TextView) view
+                              .findViewById(R.id.list_v1)).getText()
+                              .toString();
+
+                new ConsultaEventos.Servicio_imagen().execute();
+            }
+        });
+
         return myview;
     }
 
@@ -59,7 +76,6 @@ public class ConsultaEventos extends android.app.Fragment implements View.OnClic
         Button boton = (Button) view;
 
     }
-
 
     private class Servicio_validar extends AsyncTask<Void, Void, Void> {
 
@@ -124,6 +140,74 @@ public class ConsultaEventos extends android.app.Fragment implements View.OnClic
             if (ok) {
                 v_lista.setAdapter(adapter);
                 obj_funciones.MensajeToast(context,mensaje_rs);
+            }else{
+                obj_funciones.MensajeToast(context,mensaje);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+        }
+    }
+
+    private class Servicio_imagen extends AsyncTask<Void, Void, Void> {
+
+        private boolean ok;
+        private String  mensaje_rs;
+        private String  imagen;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            ok = false;
+            ServiceHandler servicio = new ServiceHandler();
+            ArrayList<NameValuePair> parametros = new ArrayList<NameValuePair>();
+            parametros.add(new BasicNameValuePair(Constantes.KEY,Constantes.AUTENTIFICACION));
+            parametros.add(new BasicNameValuePair(Constantes.CT_EVENTO_COD_EVENTO,cod_evento));
+
+            mensaje = "Error Desconocido.";
+
+            try {
+                String jsonStr = servicio.makeServiceCall(Constantes.HOST,Constantes.WS_CS_IMAGEN,Constantes.MODO_SERVICIO,parametros);
+                if (jsonStr != null) {
+                    try {
+                        JSONObject jsonObj = new JSONObject(jsonStr);
+                        AjaxResultado resultado = servicio.fn_json_to_has_map(jsonObj,null,2);
+                        if(resultado.getErrores() != null){
+                            mensaje = resultado.getErrores();
+                        }else if(resultado.getResultado() != null){
+                            mensaje_rs = resultado.getMensajes();
+                            imagen = resultado.getResultado();
+                            ok = true;
+                        }else{
+                            mensaje = null;
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Log.e("JSON ",jsonStr);
+                        mensaje = "Ocurrio un error en los datos JSON.";
+                    }
+                }else{
+                    mensaje = "El servidor no retorno la respuesta.";
+                }
+            }  catch (Exception e) {
+                e.printStackTrace();
+                mensaje = "Error.";
+            }
+            publishProgress();
+            return null;
+        }
+
+        protected void onProgressUpdate (Void... valores) {
+            if (ok) {
+
             }else{
                 obj_funciones.MensajeToast(context,mensaje);
             }
